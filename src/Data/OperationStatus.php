fe9174e284b6ca9074836a49d41e3381eccb5794
Christian LeVesque <christian@levesque.dev>
2024-11-26 16:57:07 -0600

    converted OperationStatus to a backed enum for serialization

--- src/Data/OperationStatus.php
+++ src/Data/OperationStatus.php
@@ -2,40 +2,40 @@
 
 namespace Sienar\Data;
 
-enum OperationStatus
+enum OperationStatus : int
 {
 	/**
 	 * Indicates that a hook or process was successful
 	 */
-	case Success;
+	case Success = 0;
 
 	/**
 	 * Indicates that the operation failed due to a missing entity
 	 */
-	case NotFound;
+	case NotFound = 1;
 
 	/**
 	 * Indicates that the operation failed because the user lacks permissions
 	 */
-	case Unauthorized;
+	case Unauthorized = 2;
 
 	/**
 	 * Indicates that the operation failed due to the entity or request state
 	 */
-	case Unprocessable;
+	case Unprocessable = 3;
 
 	/**
 	 * Indicates that the operation failed due to a database conflict
 	 */
-	case Conflict;
+	case Conflict = 4;
 
 	/**
 	 * Indicates that the operation failed due to a database concurrency error
 	 */
-	case Concurrency;
+	case Concurrency = 5;
 
 	/**
 	 * Indicates that the operation failed for unknown reasons
 	 */
-	case Unknown;
+	case Unknown = 6;
 }

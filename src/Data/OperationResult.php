5056405b78021b9ad95e47ed926cf46768195416
Christian LeVesque <christian@levesque.dev>
2024-11-26 16:50:39 -0600

    added OperationResult enum to represent the full result of an operation

---
+++ src/Data/OperationResult.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sienar\Data;
+
+readonly class OperationResult
+{
+	private OperationStatus $status;
+	private mixed $result;
+	private ?string $message;
+
+	/**
+	 * @param \Sienar\Data\OperationStatus $status
+	 * @param mixed $result
+	 * @param string|null $message
+	 */
+	public function __construct(
+		OperationStatus $status = OperationStatus::Success,
+		mixed $result = null,
+		?string $message = null)
+	{
+		$this->status = $status;
+		$this->result = $result;
+		$this->message = $message;
+	}
+
+	public function getStatus(): OperationStatus
+	{
+		return $this->status;
+	}
+
+	public function getResult(): mixed
+	{
+		return $this->result;
+	}
+
+	public function getMessage(): ?string
+	{
+		return $this->message;
+	}
+}

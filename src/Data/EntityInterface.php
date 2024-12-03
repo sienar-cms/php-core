50a79d3e22f29e54decb377024324e3a451d3002
Christian LeVesque <christian@levesque.dev>
2024-12-03 11:33:26 -0600

    updated entities to use Uuid keys instead of int keys

--- src/Data/EntityInterface.php
+++ src/Data/EntityInterface.php
@@ -2,6 +2,8 @@
 
 namespace Sienar\Data;
 
+use Symfony\Component\Uid\Uuid;
+
 /**
  * A base interface containing the getters and setters required by all entities in the app
  */
@@ -9,23 +11,23 @@ interface EntityInterface {
 	/**
 	 * Represents the unique ID of the entity
 	 *
-	 * @return int|null
+	 * @return Uuid|null
 	 */
-	public function getId(): ?int;
+	public function getId(): ?Uuid;
 
 	/**
 	 * Represents the concurrency stamp of the entity
 	 *
-	 * @return int|null
+	 * @return Uuid|null
 	 */
-	public function getConcurrencyStamp(): ?int;
+	public function getConcurrencyStamp(): ?Uuid;
 
 	/**
 	 * Sets the new concurrency stamp of the entity
 	 *
-	 * @param int $concurrencyStamp The new concurrency stamp
+	 * @param Uuid $concurrencyStamp The new concurrency stamp
 	 *
 	 * @return void
 	 */
-	public function setConcurrencyStamp(int $concurrencyStamp): void;
+	public function setConcurrencyStamp(Uuid $concurrencyStamp): void;
 }
--- src/Data/RepositoryInterface.php
+++ src/Data/RepositoryInterface.php
@@ -2,21 +2,23 @@
 
 namespace Sienar\Data;
 
+use Symfony\Component\Uid\Uuid;
+
 /**
  * Exposes methods for storing, updating, and retrieving instances of an entity from a datastore
  *
- * @template T
+ * @template T of EntityInterface
  */
 interface RepositoryInterface {
 	/**
 	 * Reads an entity from the datastore
 	 *
-	 * @param int $id The ID of the entity
+	 * @param Uuid $id The ID of the entity
 	 * @param Filter|null $filter A {@see Filter} to specify included results
 	 *
 	 * @return T|null The entity if it exists, else <code>null</code>
 	 */
-	public function read(int $id, ?Filter $filter = null): ?EntityInterface;
+	public function read(Uuid $id, ?Filter $filter = null): ?EntityInterface;
 
 	/**
 	 * Reads all entities from the datastore that satisfy the provided {@see Filter}
@@ -32,9 +34,9 @@ interface RepositoryInterface {
 	 *
 	 * @param T $entity The entity to create
 	 *
-	 * @return int|null The entity's primary key if successful, else <code>null</code>
+	 * @return Uuid|null The entity's primary key if successful, else <code>null</code>
 	 */
-	public function create(EntityInterface $entity): ?int;
+	public function create(EntityInterface $entity): ?Uuid;
 
 	/**
 	 * Updates an existing entity in the datastore
@@ -48,18 +50,18 @@ interface RepositoryInterface {
 	/**
 	 * Deletes an entity from the datastore
 	 *
-	 * @param int $id The primary key of the entity to delete
+	 * @param Uuid $id The primary key of the entity to delete
 	 *
 	 * @return bool Whether the delete operation was successful
 	 */
-	public function delete(int $id): bool;
+	public function delete(Uuid $id): bool;
 
 	/**
 	 * Reads the concurrency stamp for the entity with the given ID
 	 *
-	 * @param int $id The ID of the entity
+	 * @param Uuid $id The ID of the entity
 	 *
-	 * @return int|null The concurrency stamp of the entity if it exists, else <code>null</code>
+	 * @return Uuid|null The concurrency stamp of the entity if it exists, else <code>null</code>
 	 */
-	public function readConcurrencyStamp(int $id): ?int;
+	public function readConcurrencyStamp(Uuid $id): ?Uuid;
 }

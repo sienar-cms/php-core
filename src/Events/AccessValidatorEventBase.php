046ef05a7d00ac5a8954ba3332fded10eb9d62fa
Christian LeVesque <christian@levesque.dev>
2024-12-04 17:00:09 -0600

    added PHPDoc to AccessValidatorEventBase

--- src/Events/AccessValidatorEventBase.php
+++ src/Events/AccessValidatorEventBase.php
@@ -4,6 +4,12 @@ namespace Sienar\Events;
 
 use Sienar\Infrastructure\AccessValidationContext;
 
+/**
+ * A base type for events that are fired to validate access to a process or entity
+ *
+ * @template T The type of the event target
+ * @extends TargetedEventBase<T>
+ */
 abstract class AccessValidatorEventBase extends TargetedEventBase {
 	private readonly AccessValidationContext $context;
 
@@ -15,6 +21,11 @@ abstract class AccessValidatorEventBase extends TargetedEventBase {
 		$this->context = $context;
 	}
 
+	/**
+	 * Gets the event's {@see AccessValidationContext}
+	 *
+	 * @return AccessValidationContext
+	 */
 	public function getAccessValidationContext(): AccessValidationContext {
 		return $this->context;
 	}
